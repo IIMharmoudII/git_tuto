fbaab821551c8ebd078bdf9fd4f31eff6b454814
IIMharmoudII <159728877+IIMharmoudII@users.noreply.github.com>
2024-12-25 17:57:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,15 +30,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Le bot Smash or Pass est en ligne !"
-
-@app.route('/health')
-def health():
-    return "OK", 200
+    return "Le bot est en ligne !"
 
 def run():
-    port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
